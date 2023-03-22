119d62ea9ef753810a43012292e9fd697f039238
Coleman Dill <cdill@srv-izumi.teslamotors.com>
2023-03-22 16:11:55 -0500

    more telescope keybindings

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -54,3 +54,11 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.keymap.set('n', '<leader>fr', builtin.resume, {})
+vim.keymap.set('n', '<leader>fp', builtin.builtin, {})
+
+vim.keymap.set('n', '<leader>ls', builtin.lsp_dynamic_workspace_symbols, {})
+vim.keymap.set('n', '<leader>lr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
