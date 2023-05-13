f952873e916f1ca3177787e380043b98a582510e
Coleman <contact@cdill.net>
2023-05-13 17:15:42 -0500

    mpris keybinds

--- nvim/init.lua
+++ nvim/init.lua
@@ -70,7 +70,7 @@ for k, v in pairs(options) do
 end
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = {"c", "cpp", "python", "lua", "rust"},
+  ensure_installed = {"c", "cpp", "python", "lua", "rust", "nix"},
   highlight = {
     enable = true,
   },
